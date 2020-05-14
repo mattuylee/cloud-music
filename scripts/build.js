a4bb742374c2710299b91bc0829e9b4a68f321c2
Mattuy Lee <mattuy@163.com>
2020-05-14 20:25:17 +0800

    chore(webpack): 设置生产环境的publicPath为./

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,9 @@
 'use strict';
 
+//设置base href
+process.env.PUBLIC_URL = './'
+
+
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
