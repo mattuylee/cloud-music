15152ea16c07ddaa8ddf427b8622c594ed48c0b6
Mattuy Lee <mattuy@163.com>
2020-05-16 00:12:04 +0800

    daily: 推荐歌单列表

--- src/application/recommend/index.js
+++ src/application/recommend/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import Slider from '../../components/slider/slider';
+import RecommendList from '../../components/list';
 
-// eslint-disable-next-line
-const bannerList = [1,2,3,4].map (item => {
-  return { imageUrl: "http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg?_t="+item }
+const bannerList = [1, 2, 3, 4].map(item => {
+  return { imageUrl: "http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg?_t=" + item }
+});
+const recommendList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(item => {
+  return {
+    id: item,
+    picUrl: "https://p1.music.126.net/fhmefjUfMD-8qtj3JKeHbA==/18999560928537533.jpg",
+    playCount: 17171122,
+    name: "朴树、许巍、李健、郑钧、老狼、赵雷"
+  }
 });
 
 function Recommend() {
-
   return (
-    <Slider bannerList={bannerList}></Slider>
+    <>
+      <Slider bannerList={bannerList}></Slider>
+      <RecommendList recommendList={recommendList}></RecommendList>
+    </>
   );
 }
 
---
+++ src/components/list/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { getCount } from '../../api/util';
+import * as css from './list.module.less';
+
+function RecommendList(props) {
+  const { recommendList } = props
+  return (
+    <>
+      <label className={css.label}>推荐歌单</label>
+      <ul className={css.list}>
+        {recommendList.map((item) => (
+          <li key={item.id} className="list-item">
+            <img src={item.picUrl + '?param=300x300'} width="100%" alt="推荐歌单" />
+            <p className="description">{item.name}</p>
+            <span className="play-count">
+              <i className="iconfont icon-headset"></i>
+              {getCount(item.playCount)}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default React.memo(RecommendList);
